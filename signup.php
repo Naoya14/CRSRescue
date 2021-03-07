b17786bb07882e1d4c29acfcd9fce972175cc555
Naoya Tabata <naoya1059@gmail.com>
2021-03-07 17:45:17 +0900

    implement signup.php

--- signup.php
+++ signup.php
@@ -1,33 +1,34 @@
 <?php
 
-if(isset($_POST['login'])){
+if(isset($_POST['signup'])){
   // form handling
-  $username = $_POST['usernametype'];
-  $password =  $_POST['passwordtype'];
-  $email = $_POST['emailtype'];
-  $fullname = $_POST['fullnametype'];
-  $usertypes = $_POST['usertype'];
+  $username = $_POST['username'];
+  $password =  $_POST['password'];
+  $name = $_POST['name'];
+  $phone = $_POST['phone'];
+  $conntry = $_POST['country'];
+  $gender = $_POST['gender'];
 }
 
 // 1. DB Server connection
-  $con = mysqli_connect('localhost', 'root', '', 'mehahospital');
+  $con = mysqli_connect('localhost', 'root', '', 'crsrescue_db');
 // check the connection
 if($con->connect_error)
     die(" DB Connection failed " . $con->connect_error);
 
 //Check existing user
-$query ="SELECT * FROM signupuser WHERE username='$username';";
+$query ="SELECT * FROM tb_volunteers WHERE username='$username';";
 $result = mysqli_query($con,$query);
  if(mysqli_num_rows($result)>0){
-     echo '<script>alert("Userename already exist.");window.location = "index.php";</script>';
+     echo '<script>alert("Userename already exist.");window.location = "signup.html";</script>';
 
 }else{
   // Insert the values into database table users
-  $sqlQuery = "INSERT INTO signupuser VALUES ( '$username', '$password','$email','$fullname', '$usertypes')";
+  $sqlQuery = "INSERT INTO tb_volunteers VALUES ( '$username', '$password','$name','$phone', '$gender', '$country')";
 
   // Execute the query
   if ($con->query($sqlQuery) == TRUE ) {
-    echo '<script>alert("Sign up successful");window.location = "index.php";</script>';
+    echo '<script>alert("Sign up successful");window.location = "index.html";</script>';
   }
   else {
     echo "<script>alert('sign up failed');</script>";
