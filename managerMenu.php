9d4acb11716eb1e917cfe964792fd4463395061c
kelvin1810 <fishaputrakelvin1810@gmail.com>
2021-03-21 17:00:25 +0800

    signout.php

--- managerMenu.php
+++ managerMenu.php
@@ -67,7 +67,7 @@ if(isset($_SESSION['m_login']) == false)
           <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
 
             <li class="nav-item">
-              <a class="nav-link" href="#">Sign out</a>
+              <a class="nav-link" href="signout.php">Sign out</a>
             </li>
           </ul>
         </div>
