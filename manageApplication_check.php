1e2c42f25a34c5b0c6edd516ce5c0e880acc3228
kelvin1810 <fishaputrakelvin1810@gmail.com>
2021-03-28 17:50:32 +0800

    try

--- manageApplication_check.php
+++ manageApplication_check.php
@@ -10,42 +10,24 @@ try
   $dbh = new PDO($dsn, $db_user, $db_password);
   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $id = $_GET['id'];
-  $username = $_GET['username'];
-  $applicationDate = date("Y/m/d");
-  $status = 'New';
+  $applicationID = $_GET['applicationID'];
+  $status = $_GET['status'];
+  $remark = $_GET['remark'];
 
-  $id = htmlspecialchars($id, ENT_QUOTES, 'UTF-8');
-  $username = htmlspecialchars($username, ENT_QUOTES, 'UTF-8');
+  $applicationID = htmlspecialchars($applicationID, ENT_QUOTES, 'UTF-8');
+  $status = htmlspecialchars($status, ENT_QUOTES, 'UTF-8');
+  $remark = htmlspecialchars($remark, ENT_QUOTES, 'UTF-8');
 
-  $sql_checkapplication = 'SELECT applicationID FROM tb_applications WHERE username_volunteer=? AND tripID=?';
-  $prepare = $dbh->prepare($sql_checkapplication);
-  $prepare->bindValue(1, $_SESSION['username'], PDO::PARAM_STR);
-  $prepare->bindValue(2, $id, PDO::PARAM_STR);
-  $prepare->execute();
-
-  $result = $prepare->fetch(PDO::FETCH_ASSOC);
-
-  if($result==false)
-  {
-    $sql = 'INSERT INTO tb_applications(applicationDate, status, username_staff, username_volunteer, tripID) VALUES (?, ?, ?, ?, ?)';
+    $sql = 'INSERT INTO tb_applications( status,remark) VALUES (?, ?,)';
     $prepare = $dbh->prepare($sql);
-    $prepare->bindValue(1, $applicationDate, PDO::PARAM_STR);
-    $prepare->bindValue(2, $status, PDO::PARAM_STR);
-    $prepare->bindValue(3, $username, PDO::PARAM_STR);
-    $prepare->bindValue(4, $_SESSION['username'], PDO::PARAM_STR);
-    $prepare->bindValue(5, $id, PDO::PARAM_STR);
-
+    $prepare->bindValue(1, $status, PDO::PARAM_STR);
+    $prepare->bindValue(2, $remark, PDO::PARAM_STR);
     $prepare->execute();
 
     $dbh = null;
 
-    echo '<script>alert("You apply for the trip successfully");window.location = "volunteerMenu.php";</script>';
-  }
-  else
-  {
-    echo '<script>alert("You already applied for this trip");window.location = "applyTrip.php";</script>';
-  }
+    echo '<script>alert("You updated the application successfully");window.location = "manageApplication.php";</script>';
+
 }
 catch (Exception $e)
 {
