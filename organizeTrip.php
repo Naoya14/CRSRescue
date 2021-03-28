09cb869fd54e638351658cf6b27252a10462edad
kelvin1810 <fishaputrakelvin1810@gmail.com>
2021-03-28 14:02:57 +0800

    link-forgot

--- organizeTrip.php
+++ organizeTrip.php
@@ -47,7 +47,7 @@ if(isset($_SESSION['s_login']) == false)
       <div class="list-group list-group-flush">
         <a href="staffMenu.php" class="list-group-item list-group-item-action bg-light">Dashboard</a>
         <a href="organizeTrip.php" class="list-group-item list-group-item-action bg-light">Organize Trip</a>
-        <a href=".php" class="list-group-item list-group-item-action bg-light"> Manage Application</a>
+        <a href="manageApplication.php" class="list-group-item list-group-item-action bg-light"> Manage Application</a>
       </div>
     </div>
     <!-- /#sidebar-wrapper -->
