c6774ac68f531975ea8aa1bbc44612048497bea3
Naoya Tabata <naoya1059@gmail.com>
2021-03-29 18:43:38 +0900

    Update applyTrip.php
    Fixed bag that don't show the trips which no one apply for yet

--- applyTrip.php
+++ applyTrip.php
@@ -14,9 +14,9 @@ $db_password = "";
 $dbh = new PDO($dsn, $db_user, $db_password);
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$sql = 'SELECT *, count(tb_trips.tripID) AS count 
-FROM tb_trips JOIN tb_applications ON tb_trips.tripID=tb_applications.tripID GROUP BY tb_trips.tripID
-HAVING numVolunteers >= count';
+$sql = 'SELECT tb_trips.tripID, description, tripDate, location, numVolunteers, crisisType, count(tb_trips.tripID) AS count 
+FROM tb_trips LEFT JOIN tb_applications ON tb_trips.tripID=tb_applications.tripID GROUP BY tb_trips.tripID
+HAVING numVolunteers > count';
 $prepare = $dbh->prepare($sql);
 $prepare->execute();
 $result = $prepare->fetchAll(PDO::FETCH_ASSOC);
